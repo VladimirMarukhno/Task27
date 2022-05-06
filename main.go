c1c74771f3f6cc71c824d563cc6875e1bf0c0e70
VladimirMarukhno <Marukhno1989@mail.ru>
2022-05-06 21:51:14 +0300

    printStudent

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func newStudent() {
 		fmt.Println("Введите через пробел имя студента, его возраст и курс.")
 		n, err := fmt.Scan(&a.Name, &a.Age, &a.Grade)
 		if n == 0 {
+			printStudent()
 			break
 		} else if err != nil {
 			fmt.Println("Ошибка:", err)
@@ -24,6 +25,9 @@ func newStudent() {
 
 func main() {
 	newStudent()
+}
+
+func printStudent() {
 	fmt.Println("Данные студентов:")
 	for key, value := range studentMap {
 		fmt.Printf("%-9s %-3v %v\n", key, value.Age, value.Grade)
