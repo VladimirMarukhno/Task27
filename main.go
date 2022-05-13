21a4bb2a36b5ea25d76d14ea0d11c1a2a1dab69f
VladimirMarukhno <Marukhno1989@mail.ru>
2022-05-13 15:00:38 +0300

    Создание типа мап и добовлениея методо Put и Get

--- main.go
+++ main.go
@@ -5,31 +5,38 @@ import (
 	"fmt"
 )
 
-var studentMap = make(map[string]*Struct.Student, 10)
+type stydentMap map[string]*Struct.Student
+
+func (s stydentMap) Put(a Struct.Student) {
+	s[a.Name] = &a
+}
+
+func (s stydentMap) Get() {
+	fmt.Println("Данные студентов:")
+
+	for _, value := range s {
+		fmt.Printf("%-9s %-3v %v\n", value.Name, value.Age, value.Grade)
+	}
+}
 
 func newStudent() {
+	stydent := make(stydentMap)
 	for {
 		a := Struct.Student{}
 		fmt.Println("Введите через пробел имя студента, его возраст и курс.")
 		n, err := fmt.Scan(&a.Name, &a.Age, &a.Grade)
 		if n == 0 {
-			printStudent()
+			stydent.Get()
+			//printStudent()
 			break
 		} else if err != nil {
 			fmt.Println("Ошибка:", err)
 			break
 		}
-		studentMap[a.Put()] = &a
+		stydent.Put(a)
 	}
 }
 
 func main() {
 	newStudent()
-}
-
-func printStudent() {
-	fmt.Println("Данные студентов:")
-	for _, value := range studentMap {
-		fmt.Printf("%-9s %-3v %v\n", value.Name, value.Age, value.Grade)
-	}
 }
